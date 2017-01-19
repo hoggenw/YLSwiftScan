0de66b5487a4fb7037b4ea3c219a2f47ad5d63bd
dev-wangliugen <253192463@qq.com>
2017-01-19 17:10:32 +0800

    0.0.7

--- YLSwiftScan/Classes/YLScanViewSytle.swift
+++ YLSwiftScan/Classes/YLScanViewSytle.swift
@@ -67,7 +67,7 @@ public struct YLScanViewSytle {
      *  动画效果的图像，如线条或网格的图像
      */
     public var animationImage:UIImage? = YLScanViewSetting.imageFromBundleWithName(name: "qrcode_Scan_weixin_Line")
-   //UIImage(named: "YLScanIcon.bundle/qrcode_Scan_weixin_Line")
+
     
     
     // MARK: -非识别区域颜色,默认 RGBA (0,0,0,0.5)，范围（0--1）
